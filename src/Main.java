0cfee135fe8c877ed9b187a2f8c8b173b093bb87
Cunoucu <S5198151@studenti.unige.it>
2023-11-16 15:11:16 +0100

    D.D.D.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("area: " + f1.area() + " perimentro: " + f1.perimetro());
         Forma f2 = new Rettangolo(10, 5);
         System.out.println("area: " + f2.area() + " perimentro: " + f2.perimetro());
-        Forma f3 = new Quadrato(10);
+        Forma f3 = new Quadrato(f1.perimetro()*10);
         System.out.println("area: " + f3.area() + " perimentro: " + f3.perimetro());
     }
 }
